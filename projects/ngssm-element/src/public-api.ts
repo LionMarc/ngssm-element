7e081ecd4476dde93d9c6b29b09b455b3a5e70dc
Marc LION <lionmarc@free.fr>
2023-09-23 14:36:12 +0200

    Add ngssm-event-bus for communicaton between custom elements (#9)

--- projects/ngssm-element/src/public-api.ts
+++ projects/ngssm-element/src/public-api.ts
@@ -8,3 +8,4 @@ export * from './lib/ngssm-elements-loader.service';
 export * from './lib/with-access-token';
 export * from './lib/access-token-store';
 export * from './lib/oauth.interceptor';
+export * from './lib/ngssm-event-bus';
