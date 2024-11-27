1defdb5118273d0138c23bab1c968e531519992d
Marc LION <lionmarc@free.fr>
2024-11-27 07:47:11 +0100

    Fix injection error when using elements with token provider

--- projects/ngssm-element/src/lib/provide-ngssm-element.ts
+++ projects/ngssm-element/src/lib/provide-ngssm-element.ts
@@ -1,16 +1,16 @@
 import {
   EnvironmentProviders,
   InjectionToken,
-  Type,
   makeEnvironmentProviders,
   inject,
   provideAppInitializer
 } from '@angular/core';
 import { Observable, filter } from 'rxjs';
 
+import { Logger } from 'ngssm-store';
+
 import { NgssmEvent, NgssmEventBus } from './ngssm-event-bus';
 import { AccessTokenStore } from './access-token-store';
-import { Logger } from 'ngssm-store';
 
 export const requestAccessTokenEvent = 'request-access-token';
 export const getRequestAccessTokenEvent = (): NgssmEvent => ({
@@ -26,22 +26,18 @@ export const getAccessTokenEvent = (token: string): AccessTokenEvent => ({
   token
 });
 
-export const requestAndStoreAccessToken = (
-  eventBus: NgssmEventBus,
-  accessTokenStore: AccessTokenStore
-): (() => void) => {
-  return () => {
-    eventBus.event$.pipe(filter((e) => e.type === accessTokenEvent)).subscribe((e) => {
-      accessTokenStore.accessToken = (e as AccessTokenEvent).token;
-    });
-    eventBus.publish(getRequestAccessTokenEvent());
-  };
+export const requestAndStoreAccessToken = () => {
+  const eventBus = inject(NgssmEventBus);
+  const accessTokenStore = inject(AccessTokenStore);
+
+  eventBus.event$.pipe(filter((e) => e.type === accessTokenEvent)).subscribe((e) => {
+    accessTokenStore.accessToken = (e as AccessTokenEvent).token;
+  });
+  eventBus.publish(getRequestAccessTokenEvent());
 };
 
 export const provideNgssmElementForElementsProvider = (): EnvironmentProviders => {
-  return makeEnvironmentProviders([
-    provideAppInitializer(requestAndStoreAccessToken(inject(NgssmEventBus), inject(AccessTokenStore)))
-  ]);
+  return makeEnvironmentProviders([provideAppInitializer(requestAndStoreAccessToken)]);
 };
 
 export interface AccessTokenProvider {
@@ -60,42 +56,38 @@ export const publishAccessToken = (
   eventBus.publish(getAccessTokenEvent(token));
 };
 
-export const publishAccessTokenFactory = (): (() => void) => {
+export const publishAccessTokenFactory = () => {
   const eventBus = inject(NgssmEventBus);
   const accessTokenProvider: AccessTokenProvider = inject(NGSSM_ACCESS_TOKEN_PROVIDER);
   const accessToken: Observable<string> = inject(NGSSM_ACCESS_TOKEN_OBSERVABLE);
   const accessTokenStore = inject(AccessTokenStore);
   const logger = inject(Logger);
-  return () => {
-    publishAccessToken(eventBus, accessTokenProvider, accessTokenStore);
-    eventBus.event$.pipe(filter((e) => e.type === requestAccessTokenEvent)).subscribe(() => {
-      publishAccessToken(eventBus, accessTokenProvider, accessTokenStore);
-    });
 
-    accessToken.subscribe((a) => {
-      logger.information(`[publishAccessTokenFactory] New access token notified`);
-      accessTokenStore.accessToken = a;
-      eventBus.publish(getAccessTokenEvent(a));
-    });
-  };
+  publishAccessToken(eventBus, accessTokenProvider, accessTokenStore);
+  eventBus.event$.pipe(filter((e) => e.type === requestAccessTokenEvent)).subscribe(() => {
+    publishAccessToken(eventBus, accessTokenProvider, accessTokenStore);
+  });
+
+  accessToken.subscribe((a) => {
+    logger.information(`[publishAccessTokenFactory] New access token notified`);
+    accessTokenStore.accessToken = a;
+    eventBus.publish(getAccessTokenEvent(a));
+  });
 };
 
 export const provideNgssmElementForElementsHost = (
-  accessTokenProviderType: Type<AccessTokenProvider>,
-  accessTokenObservableFactory: () => () => Observable<string>,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  factoryDeps?: any[]
+  accessTokenProviderFactory: () => AccessTokenProvider,
+  accessTokenObservableFactory: () => Observable<string>
 ): EnvironmentProviders => {
   return makeEnvironmentProviders([
     {
       provide: NGSSM_ACCESS_TOKEN_PROVIDER,
-      useClass: accessTokenProviderType
+      useFactory: accessTokenProviderFactory
     },
     {
       provide: NGSSM_ACCESS_TOKEN_OBSERVABLE,
-      useFactory: accessTokenObservableFactory,
-      deps: factoryDeps
+      useFactory: accessTokenObservableFactory
     },
-    provideAppInitializer(publishAccessTokenFactory())
+    provideAppInitializer(publishAccessTokenFactory)
   ]);
 };
