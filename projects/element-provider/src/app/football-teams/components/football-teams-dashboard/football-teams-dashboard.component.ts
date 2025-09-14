3af8a280aa352dd90fec11f796daab2aed37efa3
Marc LION <marclion7159@gmail.com>
2025-09-14 10:41:12 +0200

    Upgrade dependencies (#31)

--- projects/element-provider/src/app/football-teams/components/football-teams-dashboard/football-teams-dashboard.component.ts
+++ projects/element-provider/src/app/football-teams/components/football-teams-dashboard/football-teams-dashboard.component.ts
@@ -1,10 +1,9 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, inject, OnDestroy } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { takeUntil } from 'rxjs';
 
-import { ConsoleAppender, Logger, NgSsmComponent, Store } from 'ngssm-store';
+import { ConsoleAppender, Logger, Store } from 'ngssm-store';
 import { NgssmCachesDisplayButtonComponent } from 'ngssm-remote-data';
 import { NgssmEventBus } from 'ngssm-element';
 
@@ -15,24 +14,24 @@ import { NgssmEventBus } from 'ngssm-element';
   styleUrls: ['./football-teams-dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FootballTeamsDashboardComponent extends NgSsmComponent {
-  constructor(
-    store: Store,
-    consoleAppender: ConsoleAppender,
-    private eventBus: NgssmEventBus,
-    private logger: Logger
-  ) {
-    super(store);
+export class FootballTeamsDashboardComponent implements OnDestroy {
+  private readonly store = inject(Store);
+  private readonly consoleAppender = inject(ConsoleAppender);
+  private readonly eventBus = inject(NgssmEventBus);
+  private readonly logger = inject(Logger);
 
-    consoleAppender.start('football-teams');
+  constructor() {
+    this.consoleAppender.start('football-teams');
 
-    this.unsubscribeAll$.subscribe(() => consoleAppender.stop());
-
-    this.eventBus.event$.pipe(takeUntil(this.unsubscribeAll$)).subscribe((e) => {
+    this.eventBus.event$.pipe(takeUntilDestroyed()).subscribe((e) => {
       if (e.type === 'football-team') {
         this.logger.information(`Event received`, e);
       }
     });
+  }
+
+  public ngOnDestroy(): void {
+    this.consoleAppender.stop();
   }
 
   public publishEvent(): void {
